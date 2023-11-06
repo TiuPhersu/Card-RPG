75232763e263c72feac33e0bafe03d7df4a3e4fa
Andrew Le <andrew_khang_le@hotmail.com>
2023-11-06 11:19:02 -0600

    Expand Layout Stat Screen and Add Icons

--- Script/UserInterface/CharacterInterface/2dUnitInfoInterface.gd
+++ Script/UserInterface/CharacterInterface/2dUnitInfoInterface.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@onready var RADAR_CHART = self.get_node("RadarChart")
+@onready var RADAR_CHART = self.get_node("PotentialWindow/RadarChart")
 
 func _ready():
-	RADAR_CHART.set_data(10,20,30,40,50,60)
+	pass
+#	RADAR_CHART.set_data(10,20,30,40,50,60)
--- Script/UserInterface/CharacterInterface/RadarChart.gd
+++ Script/UserInterface/CharacterInterface/RadarChart.gd
@@ -1,41 +1,107 @@
 extends Control
 
-@export var DATA: Array[float] = []
-@export var LIMITS: Array[float] = []
+@export var UNIT_DATA: Resource
+@export var LIMIT: float = 100
 @export var LINE_WIDTH: float
 @export var LINE_COLOR: Color
+@export var POLYLINE_COLOR: Color
 @export var POLYGON_COLOR: Color
 @export var POLYGON_BACKGROUND_COLOR: Color
-@export var TEXT_COLOR: Color
 
-var STATS: Array[String] = ["Constitution", "Strength", "Dexterity", "Intelligence", "Willpower", "Luck"]
-
+var ACTUAL_STAT_DATA: Dictionary = {
+	"CONSTITUTION": 0, "STRENGTH": 0, "DEXTERITY": 0,
+	"INTELLIGENCE": 0, "WILLPOWER": 0, "LUCK": 0}
+var BASE_STAT_PERCENTAGE: Dictionary = {
+	"CONSTITUTION": 0, "STRENGTH": 0, "DEXTERITY": 0,
+	"INTELLIGENCE": 0, "WILLPOWER": 0, "LUCK": 0}
+var TOTAL_STAT_PERCENTAGE: Dictionary = {
+	"CONSTITUTION": 0, "STRENGTH": 0, "DEXTERITY": 0,
+	"INTELLIGENCE": 0, "WILLPOWER": 0, "LUCK": 0}
 var PERIOD
 var SIZE = Vector2.ZERO
 var RADIUS_OFFSET: float = 200
+var MAX_STAT:float = 100
 
-func set_data(test1, test2, test3, test4, test5, test6):
-	DATA.clear()
-	DATA.append(test1)
-	DATA.append(test2)
-	DATA.append(test3)
-	DATA.append(test4)
-	DATA.append(test5)
-	DATA.append(test6)
+@onready var CON_LBL = self.get_node("Constitution/ConstitutionLabel")
+@onready var STR_LBL = self.get_node("Strength/StrengthLabel")
+@onready var DEX_LBL = self.get_node("Dexterity/DexterityLabel")
+@onready var INT_LBL = self.get_node("Intelligence/IntelligenceLabel")
+@onready var WIL_LBL = self.get_node("Willpower/WillpowerLabel")
+@onready var LUK_LBL = self.get_node("Luck/LuckLabel")
+
+func set_actual_data():
+	ACTUAL_STAT_DATA["CONSTITUTION"] = UNIT_DATA.BASE_CONSTITUTION + UNIT_DATA.ADDITIONAL_CONSTITUTION
+	ACTUAL_STAT_DATA["STRENGTH"] = UNIT_DATA.BASE_STRENGTH + UNIT_DATA.ADDITIONAL_STRENGTH
+	ACTUAL_STAT_DATA["DEXTERITY"] = UNIT_DATA.BASE_DEXTERITY + UNIT_DATA.ADDITIONAL_DEXTERITY
+	ACTUAL_STAT_DATA["INTELLIGENCE"] = UNIT_DATA.BASE_INTELLIGENCE + UNIT_DATA.ADDITIONAL_INTELLIGENCE
+	ACTUAL_STAT_DATA["WILLPOWER"] = UNIT_DATA.BASE_WILLPOWER + UNIT_DATA.ADDITIONAL_WILLPOWER
+	ACTUAL_STAT_DATA["LUCK"] = UNIT_DATA.BASE_LUCK + UNIT_DATA.ADDITIONAL_LUCK
+
+func set_percentage_data():	
+	BASE_STAT_PERCENTAGE["CONSTITUTION"] = (UNIT_DATA.BASE_CONSTITUTION / MAX_STAT) * 100
+	BASE_STAT_PERCENTAGE["STRENGTH"] = (UNIT_DATA.BASE_STRENGTH / MAX_STAT) * 100
+	BASE_STAT_PERCENTAGE["DEXTERITY"] = (UNIT_DATA.BASE_DEXTERITY / MAX_STAT) * 100
+	BASE_STAT_PERCENTAGE["INTELLIGENCE"] = (UNIT_DATA.BASE_INTELLIGENCE / MAX_STAT) * 100
+	BASE_STAT_PERCENTAGE["WILLPOWER"] = (UNIT_DATA.BASE_WILLPOWER / MAX_STAT) * 100
+	BASE_STAT_PERCENTAGE["LUCK"] = (UNIT_DATA.BASE_LUCK / MAX_STAT) * 100
+	
+	TOTAL_STAT_PERCENTAGE["CONSTITUTION"] = ((UNIT_DATA.BASE_CONSTITUTION + UNIT_DATA.ADDITIONAL_CONSTITUTION) / MAX_STAT) * 100
+	TOTAL_STAT_PERCENTAGE["STRENGTH"] = ((UNIT_DATA.BASE_STRENGTH + UNIT_DATA.ADDITIONAL_STRENGTH) / MAX_STAT) * 100
+	TOTAL_STAT_PERCENTAGE["DEXTERITY"] = ((UNIT_DATA.BASE_DEXTERITY + UNIT_DATA.ADDITIONAL_DEXTERITY) / MAX_STAT) * 100
+	TOTAL_STAT_PERCENTAGE["INTELLIGENCE"] = ((UNIT_DATA.BASE_INTELLIGENCE + UNIT_DATA.ADDITIONAL_INTELLIGENCE) / MAX_STAT) * 100
+	TOTAL_STAT_PERCENTAGE["WILLPOWER"] = ((UNIT_DATA.BASE_WILLPOWER + UNIT_DATA.ADDITIONAL_WILLPOWER) / MAX_STAT) * 100
+	TOTAL_STAT_PERCENTAGE["LUCK"] = ((UNIT_DATA.BASE_LUCK + UNIT_DATA.ADDITIONAL_LUCK) / MAX_STAT) * 100
+	
+
+func set_ranking():
+	CON_LBL.text = determine_rank(TOTAL_STAT_PERCENTAGE["CONSTITUTION"])
+	STR_LBL.text = determine_rank(TOTAL_STAT_PERCENTAGE["STRENGTH"])
+	DEX_LBL.text = determine_rank(TOTAL_STAT_PERCENTAGE["DEXTERITY"])
+	INT_LBL.text = determine_rank(TOTAL_STAT_PERCENTAGE["INTELLIGENCE"])
+	WIL_LBL.text = determine_rank(TOTAL_STAT_PERCENTAGE["WILLPOWER"])
+	LUK_LBL.text = determine_rank(TOTAL_STAT_PERCENTAGE["LUCK"])
+
+func determine_max_stat():
+	var cur_max : float = ACTUAL_STAT_DATA.values()[0]
+	for stat in ACTUAL_STAT_DATA:  
+		if ACTUAL_STAT_DATA[stat] > cur_max:  
+			cur_max = ACTUAL_STAT_DATA[stat]
+	MAX_STAT = cur_max
+
+func determine_rank(value):
+	var rank
+	if value >= 80:
+		rank = "A"
+	elif value >= 60 and value < 80:
+		rank = "B"
+	elif value >= 40 and value < 60:
+		rank = "C"
+	elif value >= 20 and value < 40:
+		rank = "D"
+	elif value >= 0 and value < 20:
+		rank = "F"
+	else:
+		rank = "?"
+	return rank
 
 func _ready():
-	PERIOD = 2 * PI / DATA.size()
+	set_actual_data()
+	determine_max_stat()
+	set_percentage_data()
+	set_ranking()
+	PERIOD = 2 * PI / BASE_STAT_PERCENTAGE.size()
 
 func _draw():
 	create_polygon_background()
-	create_polygon()
-	create_radar()
 	create_radar_axis()
+	create_radar()
+	create_polygon()
+	create_polyline()
 
 func create_radar():
 	var points = []
 	var radius = min(SIZE.x, SIZE.y)/2 - RADIUS_OFFSET
-	for i in DATA.size():
+	for i in BASE_STAT_PERCENTAGE.size():
 		var temp = PERIOD * i;
 		var way = Vector2(cos(temp), sin(temp))
 		points.append(way * radius + SIZE / 2)
@@ -47,9 +113,9 @@ func create_radar():
 
 func create_radar_axis():
 	var points = []
-	for x in range(1, 6):
-		var radius = min(SIZE.x, SIZE.y)/2 - (RADIUS_OFFSET * x/6)
-		for i in DATA.size():
+	for x in range(1, 5):
+		var radius = min(SIZE.x, SIZE.y)/2 - (RADIUS_OFFSET * x/5)
+		for i in BASE_STAT_PERCENTAGE.size():
 			var temp = PERIOD * i;
 			var way = Vector2(cos(temp), sin(temp))
 			points.append(way * radius + SIZE / 2)
@@ -61,7 +127,7 @@ func create_polygon_background():
 	var points = []
 	var pointsColor = []
 	var radius = min(SIZE.x, SIZE.y)/2 - RADIUS_OFFSET
-	for i in DATA.size():
+	for i in BASE_STAT_PERCENTAGE.size():
 		var temp = PERIOD * i;
 		var way = Vector2(cos(temp), sin(temp))
 		points.append(way * radius + SIZE / 2)
@@ -69,14 +135,29 @@ func create_polygon_background():
 	
 	draw_polygon(points, pointsColor)
 
+func create_polyline():
+	var points = []
+	var radius = min(SIZE.x, SIZE.y)/2 - RADIUS_OFFSET
+	var i = 0
+	for key in BASE_STAT_PERCENTAGE:
+		var valueRatio = BASE_STAT_PERCENTAGE[key] / LIMIT
+		var temp = PERIOD * i;
+		var way = Vector2(cos(temp), sin(temp))
+		points.append(way * radius * valueRatio + SIZE / 2)
+		i = i + 1
+	points.append(points[0])
+	draw_polyline(points, POLYLINE_COLOR, LINE_WIDTH, true)
+
 func create_polygon():
 	var points = []
 	var pointsColor = []
 	var radius = min(SIZE.x, SIZE.y)/2 - RADIUS_OFFSET
-	for i in DATA.size():
-		var valueRatio = DATA[i] / LIMITS[i]
+	var i = 0
+	for key in TOTAL_STAT_PERCENTAGE:
+		var valueRatio = TOTAL_STAT_PERCENTAGE[key] / LIMIT
 		var temp = PERIOD * i;
 		var way = Vector2(cos(temp), sin(temp))
 		points.append(way * radius * valueRatio + SIZE / 2)
 		pointsColor.append(POLYGON_COLOR)
+		i = i + 1
 	draw_polygon(points,pointsColor)
